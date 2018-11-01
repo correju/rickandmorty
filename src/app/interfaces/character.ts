d4c99e2749f217f670393ec09ddc98dc8ec7052b
julian correa <correa.julian@gmail.com>
2018-11-01 13:22:50 -0500

    Added interfaces

---
+++ src/app/interfaces/character.ts
@@ -0,0 +1,10 @@
+import { Location } from './location';
+export interface Character {
+  image: string,
+  name: string,
+  origin: Location,
+  species: string,
+  status: string,
+  location: Location,
+  gender: string
+}
---
+++ src/app/interfaces/characters.ts
@@ -0,0 +1,4 @@
+export interface Characters {
+  info: any,
+  results: any
+}
